67aa5da7befc5f0630a305b86bc214c12e415549
Greg Duarte <gregduarte97@gmail.com>
2019-02-25 22:24:15 -0800

    Added Cinemachine, moving Player, GameManager, prefabs, etc
      + Created Player script and prefab, operates like a basic 2D platform controller via dynamic Rigidbody2D and velocity setting
      + Added Cinemachine package, childed a Cinemachine 2D Camera to Player prefab
      + Created a skeleton GameManager singleton, and prefabbed a GameObject for it
      * Bumped up Physics2D.gravity to (0, -25) for tighter 2D physics fun
      * Added Player tag to Tag Manager
      - Removed useless SampleScene.unity

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        // Singleton pattern
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
