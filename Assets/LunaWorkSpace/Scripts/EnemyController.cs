0ba9b0fcd49522bfacf8fa0c7d1fe34f1691d760
Luis Luna <lfluna@uci.edu>
2019-02-26 17:40:25 -0800

    Build Scene
    -Added prefabs to Greg scene
    -Added enemies that patrol
    -Added stats for enemies
    -Added seasoning pickup behavior
    -changed size

---
+++ Assets/LunaWorkSpace/Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Stats")]
+    public GameObject agent; //GameObject in scene
+    public Stats stats; //Scrip Obj with Stats
+
+    [Header("Patrol")]
+    public List<Transform> patrolLocations;
+    public int nextPatrolLocation;
+
+    
+}
