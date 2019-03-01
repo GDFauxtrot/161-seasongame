caa3cf913e5f985edb5e8802a47e282a4b205ee9
Luis Luna <lfluna@uci.edu>
2019-02-28 23:50:52 -0800

    Layout change for Shopper Enemy

---
+++ Assets/LunaWorkSpace/Scripts/Shopper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Shopper : MonoBehaviour
+{
+    [Header("Stats")]
+    public float speed;
+    public float slowDownAmount;
+    public float smoothness;
+
+    [Header("Physics")]
+    public Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        rb2D.velocity = new Vector2(Mathf.Lerp(rb2D.velocity.x, speed, smoothness), 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+            speed = -speed;
+        
+    }
+}
