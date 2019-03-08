f4e2bf3b5f770a712c94d638d536d5ffe0a6d3e0
seantsukim <seantsukim@gmail.com>
2019-03-08 15:40:24 -0800

    changed one small variable
    wait for seconds changed to .1f instead of 1f makes the start of the game look a bit smoother.

--- Assets/Scripts/arrow.cs
+++ Assets/Scripts/arrow.cs
@@ -12,7 +12,7 @@ public class arrow : MonoBehaviour
     }
 
     IEnumerator Delay(){
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(.1f);
         seasoning = GameObject.FindGameObjectWithTag("seasoning");
     }
     // Update is called once per frame
