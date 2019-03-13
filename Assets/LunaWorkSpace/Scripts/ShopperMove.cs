e0ba74e3e44bb1e10d868b25117d0a5d90297547
seantsukim <seantsukim@gmail.com>
2019-03-12 19:21:05 -0700

    more polish and shopper movement
    shopper movement is added as well as some more polish for the game

---
+++ Assets/LunaWorkSpace/Scripts/ShopperMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopperMove : MonoBehaviour
+{
+    /*inspector field that allows us to randomize speed */
+    [Header("Randomize Speed")]
+    public int minSpeed = 10;
+    public int maxSpeed = 25;
+    private int moveSpeed = 0; //randomized speed
+    private int direction = 0; //direction to move -1 or 1
+    private Rigidbody2D shopperRB; //shopper's rigidBody
+    private SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = randomDirection();
+        shopperRB = GetComponent<Rigidbody2D>();
+        moveSpeed = Random.Range(minSpeed, maxSpeed);
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    int randomDirection()
+    {
+        int newDirection = Random.Range(0, 2);
+        int returnDirection = 0;
+        
+        if (newDirection == 0)
+            returnDirection = -1;
+        else if (newDirection == 1)
+            returnDirection = 1;
+
+        return returnDirection;
+    }
+
+    void flipShopper()
+    {
+        if (direction == -1)
+            sprite.flipX = true;
+        else if (direction == 1)
+            sprite.flipX = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shopperRB.velocity = new Vector3(direction * moveSpeed, shopperRB.velocity.y, 0);
+        flipShopper();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("InvisibleWall"))
+            moveSpeed *= -1;
+        
+    }
+}
